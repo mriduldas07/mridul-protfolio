83249599a217a21201b5ceb348e0ba823e69ceb5
mriduldas07 <mriduldas0325@gmail.com>
2024-06-09 21:42:42 +0600

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,56 @@
+import backend from "./backend.png";
+import close from "./close.svg";
+import creator from "./creator.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import mobile from "./mobile.png";
+import web from "./web.png";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.svg";
+import typescript from "./tech/typescript.png";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import jobit from "./jobit.png";
+
+export {
+  backend,
+  close,
+  creator,
+  css,
+  docker,
+  figma,
+  git,
+  github,
+  html,
+  javascript,
+  jobit,
+  menu,
+  meta,
+  mobile,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  shopify,
+  starbucks,
+  tailwind,
+  tesla,
+  threejs,
+  typescript,
+  web,
+};
---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import About from "./About";
+import Contact from "./Contact";
+import Feedbacks from "./Feedbacks";
+import Hero from "./Hero";
+import CanvasLoader from "./Loader";
+import Navbar from "./Navbar";
+import Tech from "./Tech";
+import Works from "./Works";
+import {
+  BallCanvas,
+  ComputersCanvas,
+  EarthCanvas,
+  StarsCanvas,
+} from "./canvas";
+
+export {
+  About,
+  BallCanvas,
+  CanvasLoader,
+  ComputersCanvas,
+  Contact,
+  EarthCanvas,
+  Feedbacks,
+  Hero,
+  Navbar,
+  StarsCanvas,
+  Tech,
+  Works,
+};
